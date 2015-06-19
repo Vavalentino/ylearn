fad84a033a0c2ae921889eee38043488fdd04800
Valentin Gutknecht <v-gutknecht@outlook.com>
2015-06-19 21:14:02 +0200

    add new content and refactor view code

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,70 +1,274 @@
 class ResultsController < ApplicationController
   def r1
+    @programming_language="Python"
+    @programming_language_image="python.png"
+    @tutorial_operator="Codecademy"
+    @tutorial_name="Codecademy"
+    @tutorial_image="codecademypython.png"
+    @tutorial_link="http://bit.ly/1AE7Zbw"
   end
   def r2
+    @programming_language="Java"
+    @programming_language_image="java.png"
+    @tutorial_operator="Udemy"
+    @tutorial_name="Udemy"
+    @tutorial_image="udemyjava.png"
+    @tutorial_link="http://bit.ly/1Ry5QCo"
   end
   def r3
+    @programming_language="HTML, CSS and Javascript"
+    @programming_language_image="html.png"
+    @tutorial_operator="Onemonth.com"
+    @tutorial_name="Onemonth HTML"
+    @tutorial_image="onemonthhtml.png"
+    @tutorial_link="http://bit.ly/1FTiOXF"
   end
   def r4
+    @programming_language="HTML, CSS and Javascript"
+    @programming_language_image="html.png"
+    @tutorial_operator="Onemonth.com"
+    @tutorial_name="Onemonth HTML"
+    @tutorial_image="onemonthhtml.png"
+    @tutorial_link="http://bit.ly/1HC4csw"
   end
   def r5
+    @programming_language="Objective-C"
+    @programming_language_image="objectivec.png"
+    @tutorial_operator="Teamtreehouse.com"
+    @tutorial_name="Teamtreehouse"
+    @tutorial_image="teamtreehouseobjectivec.png"
+    @tutorial_link="http://bit.ly/1FKa70k"
   end
   def r6
+    @programming_language="Swift"
+    @programming_language_image="swift.png"
+    @tutorial_operator="Onemonth.com"
+    @tutorial_name="Onemonth Swift"
+    @tutorial_image="onemonthswift.png"
+    @tutorial_link="http://bit.ly/1Jes21z"
   end
   def r7
+    @programming_language="Swift"
+    @programming_language_image="swift.png"
+    @tutorial_operator="Onemonth.com"
+    @tutorial_name="Onemonth Swift"
+    @tutorial_image="onemonthswift.png"
+    @tutorial_link="http://bit.ly/1KE5tp2"
   end
   def r8
+    @programming_language="Ruby"
+    @programming_language_image="ruby.png"
+    @tutorial_operator="Onemonth.com"
+    @tutorial_name="Onemonth Rails"
+    @tutorial_image="onemonthrails.png"
+    @tutorial_link="http://bit.ly/1HC44cu"
   end
   def r9
+    @programming_language="Ruby"
+    @programming_language_image="ruby.png"
+    @tutorial_operator="Onemonth.com"
+    @tutorial_name="Onemonth Rails"
+    @tutorial_image="onemonthrails.png"
+    @tutorial_link="http://bit.ly/1dDfRjw"
   end
   def r10
+    @programming_language="Ruby"
+    @programming_language_image="ruby.png"
+    @tutorial_operator="Onemonth.com"
+    @tutorial_name="Onemonth Rails"
+    @tutorial_image="onemonthrails.png"
+    @tutorial_link="http://bit.ly/1d5v4Jj"
   end
   def r11
+    @programming_language="Ruby"
+    @programming_language_image="ruby.png"
+    @tutorial_operator="Onemonth.com"
+    @tutorial_name="Onemonth Rails"
+    @tutorial_image="onemonthrails.png"
+    @tutorial_link="http://bit.ly/1Ry6nV5"
   end
   def r12
+    @programming_language="Java"
+    @programming_language_image="java.png"
+    @tutorial_operator="Teamtreehouse.com"
+    @tutorial_name="Teamtreehouse Android"
+    @tutorial_image="teamtreehouseandroid.png"
+    @tutorial_link="http://bit.ly/1KuEnxk"
   end
   def r13
+    @programming_language="Java"
+    @programming_language_image="java.png"
+    @tutorial_operator="Teamtreehouse.com"
+    @tutorial_name="Teamtreehouse Android"
+    @tutorial_image="teamtreehouseandroid.png"
+    @tutorial_link="http://bit.ly/1dDfUvJ"
   end
   def r14
+    @programming_language="Java"
+    @programming_language_image="java.png"
+    @tutorial_operator="Teamtreehouse.com"
+    @tutorial_name="Teamtreehouse Android"
+    @tutorial_image="teamtreehouseandroid.png"
+    @tutorial_link="http://bit.ly/1FlbVuq"
   end
   def r15
+    @programming_language="Sprite Kit"
+    @programming_language_image="spritekit.png"
+    @tutorial_operator="Teamtreehouse"
+    @tutorial_name="Teamtreehouse"
+    @tutorial_image="teamtreehousegame.png"
+    @tutorial_link="http://bit.ly/1EHqqqM"
   end
   def r16
+    @programming_language="Sprite Kit"
+    @programming_language_image="spritekit.png"
+    @tutorial_operator="Teamtreehouse"
+    @tutorial_name="Teamtreehouse"
+    @tutorial_image="teamtreehousegame.png"
+    @tutorial_link="http://bit.ly/1HC41NL"
   end
   def r17
+    @programming_language="Python"
+    @programming_language_image="python.png"
+    @tutorial_operator="Dataquest.io"
+    @tutorial_name="Dataquest.io"
+    @tutorial_image="dataquestpython.png"
+    @tutorial_link="http://bit.ly/1TxLCKZ"
   end
   def r18
+    @programming_language="Java"
+    @programming_language_image="java.png"
+    @tutorial_operator="Teamtreehouse.com"
+    @tutorial_name="Teamtreehouse Android"
+    @tutorial_image="teamtreehouseandroid.png"
+    @tutorial_link="http://bit.ly/1FKa98o"
   end
   def r19
+    @programming_language="Java"
+    @programming_language_image="java.png"
+    @tutorial_operator="Udemy"
+    @tutorial_name="Udemy"
+    @tutorial_image="udemyjava.png"
+    @tutorial_link="http://bit.ly/1BwgSPs"
   end
   def r20
+    @programming_language="Swift"
+    @programming_language_image="swift.png"
+    @tutorial_operator="Onemonth.com"
+    @tutorial_name="Onemonth Swift"
+    @tutorial_image="onemonthswift.png"
+    @tutorial_link="http://bit.ly/1HC4bEU"
   end
   def r21
+    @programming_language="Ruby"
+    @programming_language_image="ruby.png"
+    @tutorial_operator="Onemonth.com"
+    @tutorial_name="Onemonth Rails"
+    @tutorial_image="onemonthrails.png"
+    @tutorial_link="http://bit.ly/1QhfM0S"
   end
   def r22
+    @programming_language="Ruby"
+    @programming_language_image="ruby.png"
+    @tutorial_operator="Onemonth.com"
+    @tutorial_name="Onemonth Rails"
+    @tutorial_image="onemonthrails.png"
+    @tutorial_link="http://bit.ly/1cpobBV"
   end
   def r23
+    @programming_language="Ruby"
+    @programming_language_image="ruby.png"
+    @tutorial_operator="Onemonth.com"
+    @tutorial_name="Onemonth Rails"
+    @tutorial_image="onemonthrails.png"
+    @tutorial_link="http://bit.ly/1eqVCG2"
   end
   def r24
+    @programming_language="Java"
+    @programming_language_image="java.png"
+    @tutorial_operator="Teamtreehouse.com"
+    @tutorial_name="Teamtreehouse Android"
+    @tutorial_image="teamtreehouseandroid.png"
+    @tutorial_link="http://bit.ly/1d4aQzn"
   end
   def r25
+    @programming_language="Objective-C"
+    @programming_language_image="objectivec.png"
+    @tutorial_operator="Teamtreehouse.com"
+    @tutorial_name="Teamtreehouse iOS"
+    @tutorial_image="teamtreehouseobjectivec.png"
+    @tutorial_link="http://bit.ly/1GSIvbc"
   end
   def r26
+    @programming_language="Python"
+    @programming_language_image="python.png"
+    @tutorial_operator="Dataquest.io"
+    @tutorial_name="Dataquest.io"
+    @tutorial_image="dataquestpython.png"
+    @tutorial_link="http://bit.ly/1ImL9CU"
   end
   def r27
+    @programming_language="Python"
+    @programming_language_image="python.png"
+    @tutorial_operator="Onemonth.com"
+    @tutorial_name="Onemonth Python"
+    @tutorial_image="onemonthpython.png"
+    @tutorial_link="http://bit.ly/1MOS1MH"
   end
   def r28
+    @programming_language="Ruby"
+    @programming_language_image="ruby.png"
+    @tutorial_operator="Onemonth.com"
+    @tutorial_name="Onemonth Rails"
+    @tutorial_image="onemonthrails.png"
+    @tutorial_link="http://bit.ly/1K0J4km"
   end
   def r29
+    @programming_language="Python"
+    @programming_language_image="python.png"
+    @tutorial_operator="Dataquest.io"
+    @tutorial_name="Dataquest.io"
+    @tutorial_image="dataquestpython.png"
+    @tutorial_link="http://bit.ly/1N6gcHc"
   end
   def r30
+    @programming_language="Python"
+    @programming_language_image="python.png"
+    @tutorial_operator="Onemonth.com"
+    @tutorial_name="Onemonth Python"
+    @tutorial_image="onemonthpython.png"
+    @tutorial_link="http://bit.ly/1RcSbyB"
   end
   def r31
+    @programming_language="Java"
+    @programming_language_image="java.png"
+    @tutorial_operator="Teamtreehouse.com"
+    @tutorial_name="Teamtreehouse Android"
+    @tutorial_image="teamtreehouseandroid.png"
+    @tutorial_link="http://bit.ly/1J662rs"
   end
   def r32
+    @programming_language="Swift"
+    @programming_language_image="swift.png"
+    @tutorial_operator="Onemonth.com"
+    @tutorial_name="Onemonth Swift"
+    @tutorial_image="onemonthswift.png"
+    @tutorial_link="http://bit.ly/1HXUKUI"
   end
   def r33
+    @programming_language="how to speak tech"
+    @programming_language_image="tech.png"
+    @tutorial_operator="Onemonth.com"
+    @tutorial_name="Programming Basics for Non Programmers"
+    @tutorial_image="onemonthpfnp.png"
+    @tutorial_link="http://bit.ly/1Fp4VLQ"
   end
   def r34
+    @programming_language="Python"
+    @programming_language_image="python.png"
+    @tutorial_operator="Dataquest.io"
+    @tutorial_name="Dataquest.io"
+    @tutorial_image="dataquestpython.png"
+    @tutorial_link="http://bit.ly/1LhFdBd"
   end
 end
